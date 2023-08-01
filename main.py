cf460464011644bbc56ee11136112df924a961bc
andrewbrook <andrewbrook@qq.com>
2023-08-02 01:07:03 +0800

    update all

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
-
 from loguru import logger
 
-from tencent import tencent_video_auto_sign, tencent_video_get_vip_info
+from tencent import tencent_video_get_vip_info, tencent_video_auto_sign
 
 
 def main():
     logger.info("主程序启动了")
-    # tencent_video_auto_sign()
-    tencent_video_get_vip_info()
+    # tencent_video_auto_sign("root")
+    tencent_video_get_vip_info("root")
+
 
 if __name__ == '__main__':
     main()
