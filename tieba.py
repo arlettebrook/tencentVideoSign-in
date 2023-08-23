98af69ab056713745d2de4deae5ec659259e94dc
andrewbrook <andrewbrook@qq.com>
2023-08-23 12:23:45 +0800

    fist commit

---
+++ tieba.py
@@ -0,0 +1,9 @@
+# from __future__ import annotations
+
+from config import Config
+
+
+class Tieba:
+    def __init__(self, config: Config):
+        self.config = config
+        self.BDUSS = "lGY0RwNVRjT2dmSHhvQm9lczI3dnVRZkVuN0d3TVVwRFREMTNLVUIyR2RsbXhqSUFBQUFBJCQAAAAAAAAAAAEAAABknbjqZnlnZmZmdQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAJ0JRWOdCUVjNz"
